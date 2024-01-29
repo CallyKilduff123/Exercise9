1490175f07237746ea99ced2c7c00f16e397dbd0
cherinachung323 <cherina.chung@gmail.com>
2024-01-29 13:46:18 +0000

    added comments and removed wrong comments

--- projectile.py
+++ projectile.py
@@ -1,5 +1,5 @@
-# ans is 1.2
-# import math function
+# import math function Python has a set of built-in math functions, including an extensive math module, that allows
+# you to perform mathematical tasks on numbers.
 from math import pi, tan, cos
 
 # Setting constant to the formula
@@ -12,13 +12,15 @@ barrel_height = 1  # Height of the barrel (m)
 horizontal_distance = 0.5  # Horizontal distance travelled (m)
 initial_velocity = 44  # Initial velocity (m/s)
 
-# Adding the input
+# Adding the input for degree and converting it to theta
 deg = 80  # Elevation angle in degrees
 theta = deg * (pi / 180)
 # Calculate projectile height
-# Formula based on exercise guide. ** is used for power
+# Formula based on exercise guide.
+# ** is used for power
 height = barrel_height + horizontal_distance * tan(theta) - ((horizontal_distance ** 2) * g) / (
             2 * (initial_velocity * cos(theta)) ** 2)
 
-# Print the output the result, I have added the unit by adding in a string
+# Print the output the result
+# I added the unit by adding in a string "m"
 print("Height of the projectile:", height, "m")
